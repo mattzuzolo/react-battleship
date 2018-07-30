f820ae235f911032c5b8b1552b022a1e55f554ed
github username <github email address>
2018-07-29 23:20:42 -0400

    GameBoard refactored

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UUID from 'uuid';
 
 import GameTile from './GameTile'
 
-class GameBoard extends Component {
-
-  render(){
-    return(
-      <table className="game-board" id="all-tiles">
-        <tbody className="table-body">
-          <tr className="game-board">
-            {this.props.opponentBoard.map(gameTile =>
-              <GameTile
-                key={UUID()}
-                gameTile={gameTile}
-                alreadyAttacked={gameTile.alreadyAttacked}
-                shipPresent={gameTile.shipPresent}
-                clickHandler={this.props.clickHandler}
-              />
-          )}
-          </tr>
-        </tbody>
-      </table>
-    )
-  }
+const GameBoard = (props) => {
+  return(
+    <table className="game-board" id="all-tiles">
+      <tbody className="table-body">
+        <tr className="game-board">
+          {props.opponentBoard.map(gameTile =>
+            <GameTile
+              key={UUID()}
+              gameTile={gameTile}
+              alreadyAttacked={gameTile.alreadyAttacked}
+              shipPresent={gameTile.shipPresent}
+              clickHandler={props.clickHandler}
+            />
+        )}
+        </tr>
+      </tbody>
+    </table>
+  )
 }
 
 export default GameBoard
